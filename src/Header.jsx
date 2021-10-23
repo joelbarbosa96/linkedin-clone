d4bfce6ce8054278a175e3f84c5c9156ffb6ad3c
Joel Barbosa <joelbarbosa@gmail.com>
2021-10-23 21:20:29 +0100

    inital setup

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <h1>This is the Header</h1>
+      <div className="header__left">
+        <img src="" alt="" />
+
+        <div className="header__search">
+          {/* SearchIcon */}
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
